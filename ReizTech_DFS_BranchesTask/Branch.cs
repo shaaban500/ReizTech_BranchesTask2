b07b0842a4fbad1a388704dc3278a1ceedad3bad
Mohamed <Mohamed@DESKTOP-I4127VA>
2022-03-26 03:13:37 +0200

    Add project files.

---
+++ ReizTech_DFS_BranchesTask/Branch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReizTech_DFS_BranchesTask
+{
+    public class Branch
+    {
+        // this (id) is added to make each node is unique in the branch so as to be identified in the visited array in DFS code
+        public int id { get; set; } 
+        public List<Branch> branches { get; set; }
+
+        public Branch()
+        {
+            id = CurrentId.Id++;
+            branches = new List<Branch>();
+        }
+    }
+}
---
+++ ReizTech_DFS_BranchesTask/CurrentId.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReizTech_DFS_BranchesTask
+{
+    public static class CurrentId
+    {
+        public static int Id { get; set; }
+    }
+}
